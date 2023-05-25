97917f43940b802138905a1f7053fa7937ed0b5f
mohamedmustafa21 <mohamedmeda023@gmail.com>
2023-05-25 20:51:02 +0300

    mo

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function sideNavOpen(){
+    document.getElementById('sidenav').style.width='70%';
+    console.log('hello')
+} 
+function closeNavOpen(){
+    document.getElementById('sidenav').style.width='0%';
+}
